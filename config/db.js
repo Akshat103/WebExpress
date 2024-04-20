e36d4b0ede7b633827fe3415f4c35cc72ce492cd
akshat103 <akshattamrakar103@gmail.com>
2024-04-20 18:50:30 +0530

    fixed redis connection

--- config/db.js
+++ config/db.js
@@ -5,14 +5,16 @@ const redisClient = redis.createClient({
   url: process.env.REDIS_URL
 });
 
-redisClient.connect().catch(console.error);
+redisClient.on('error', err => console.log('Redis Client Error', err));
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
     console.log('MongoDB Connected');
+    await redisClient.connect();
+    console.log('Redis Connected');
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error.message);
+    console.error('Error connecting:', error.message);
     process.exit(1);
   }
 };
